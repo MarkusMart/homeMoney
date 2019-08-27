30bf659019f7ed10a0fab5e1a7fe9395be4dafe6
Marko <markusmart8@gmail.com>
2019-08-27 21:54:15 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wfm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'home-money';
+}
